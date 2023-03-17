fef9eb9f386bbacef5ae0d83b388c732e6cbdc50
beketamas <72067154+beketamas@users.noreply.github.com>
2023-03-17 12:24:22 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,30 +9,38 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
-namespace WpfApp1
+namespace WpfApp7
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
 		private int ballPosX = 0;
 		private int ballPosY = 0;
 		private int moveX = 20;
 		private int moveY = 20;
-
 		public MainWindow()
-		{
-			InitializeComponent();
-		}
+        {
+            InitializeComponent();
+			DispatcherTimer utemezo = new DispatcherTimer();
+			utemezo.Interval = TimeSpan.FromMilliseconds(500);
+            utemezo.Tick += Utemezo_Tick;
+			utemezo.Start();
+        }
 
+        private void Utemezo_Tick(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
 
-		private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
 		{
 
 			ThicknessAnimation igen = new ThicknessAnimation();
@@ -40,25 +48,20 @@ namespace WpfApp1
 			igen.Duration = new Duration(TimeSpan.FromMilliseconds(300));
 			ballPosX += moveX;
 
-			if (ballPosX < 0 || ballPosX + epLabda.Width > 800)
+			if (ballPosX < 0 || ballPosX + epLabda.Width > this.Width)
 			{
 				moveX = -moveX;
 			}
 
 			ballPosY += moveY;
-			if (ballPosY < 0 || ballPosY + epLabda.Height > 450)
+			if (ballPosY < 0 || ballPosY + epLabda.Height > this.Height)
 			{
 				moveY = -moveY;
 
 			}
+			//igen.RepeatBehavior = RepeatBehavior.Forever;
 			igen.To = new Thickness(ballPosX, ballPosY, 0, 0);
 			epLabda.BeginAnimation(MarginProperty, igen);
-
-
-
-
 		}
-
-		
 	}
 }
